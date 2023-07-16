c39df08d8b31b43e4421b803a2f4606c88c0dea0
biachristie <beatrizchristie@yahoo.com.br>
2023-07-16 12:32:17 -0300

    add HTML to Input

--- src/components/InputComponent/Input.component.jsx
+++ src/components/InputComponent/Input.component.jsx
@@ -1,7 +1,31 @@
-function InputComponent() {
+import { Form, Input } from "antd";
+
+function InputComponent({ 
+        label, 
+        rules, 
+        hasFeedback,
+        id, 
+        placeholder, 
+        type, 
+        onInput 
+    }) {
+
     return (
         <>
-            
+            <Form.Item
+                    className='form-field'
+                    label={ label }
+                    rules={ rules }
+                    hasFeedback={ hasFeedback }
+                >
+                    <Input
+                        className='form-input'
+                        id={ id }
+                        placeholder={ placeholder }
+                        type={ type }
+                        onInput={ onInput }
+                    /> 
+                </Form.Item>
         </>
     );
 }
