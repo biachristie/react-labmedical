a0ac771e38afb2de1302671d48f8c6f757327dc3
biachristie <beatrizchristie@yahoo.com.br>
2023-07-15 23:54:05 -0300

    add HTML and CSS to LoginForm

--- src/components/LoginFormComponent/LoginForm.component.jsx
+++ src/components/LoginFormComponent/LoginForm.component.jsx
@@ -1,10 +1,73 @@
-import './Login.styles.css'
+import React from 'react'
+import { Checkbox, Form, Input } from 'antd'
+
+import './LoginForm.component.css'
 
 function LoginForm() {
     return (
-        <>
-            
-        </>
+            <Form 
+                // form={ } 
+                name='login-form'
+                className='login-form'
+                layout='vertical'
+                initialValues={{ remember: true }}
+                // onFinish={  }
+                // onFinishFailed={ }
+                // validateMessages={ }
+                autoComplete='off'
+            >
+
+                <Form.Item
+                    className='login-form-field'
+                    label='E-mail Address'
+                    name='email'
+                    rules={[
+                        {
+                            required: true,
+                            type: 'email'
+                        }
+                    ]}
+                    hasFeedback
+                >
+                    <Input
+                        className='login-form-input'
+                        placeholder='name@example.com'
+                        type='email' 
+                    />  
+                </Form.Item>
+
+                <Form.Item
+                    className='login-form-field'
+                    label='Password'
+                    name='password'
+                    rules={[
+                        {
+                            required: true,
+                            type: 'password',
+                        }
+                    ]}
+                    hasFeedback
+                >
+                    <Input.Password 
+                        className='login-form-input'
+                        placeholder='Enter your password'
+                        minLength={ 8 }
+                        maxLength={ 12 }
+                    />
+                </Form.Item>
+
+                <div className='login-form-remember'>
+                    <Form.Item
+                        name='remember'
+                        valuePropName='checked'
+                        >
+                        <Checkbox className='login-form-checkbox'>Keep me signed in</Checkbox>
+                    </Form.Item>
+
+                    <a className='login-form-forgot' href="#">Forgot your password?</a>
+                </div>
+
+            </Form>
     );
 }
 
