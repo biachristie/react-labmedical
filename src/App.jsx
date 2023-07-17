b9589ae3049d1f772ff68e3e6180258659986598
biachristie <beatrizchristie@yahoo.com.br>
2023-07-17 19:44:47 -0300

    add RegisterPage to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { LoginPage } from './pages/Login/Login.page' 
-import { HomePage } from './pages/Home/Home.page' 
+import LoginPage from './pages/Login/Login.page' 
+import HomePage from './pages/Home/Home.page'
+import RegisterPage from './pages/Register/Register.page'
 
 function App() {
     return (
       <Router>
         <Routes>
           <Route path='/' element={ <LoginPage /> } />
+          <Route path='/register' element={ <RegisterPage /> } />
           <Route path='/home' element={ <HomePage /> } />
         </Routes>
       </Router>
