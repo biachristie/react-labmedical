69baeb5dd5ca16195b7f663e9e3e0c42fdab147c
biachristie <beatrizchristie@yahoo.com.br>
2023-07-16 21:37:01 -0300

    add title and Tooltip to LoginForm

--- src/components/LoginFormComponent/LoginForm.component.jsx
+++ src/components/LoginFormComponent/LoginForm.component.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Checkbox, Form, message } from 'antd'
+import { Checkbox, Form, Tooltip, message } from 'antd'
 
 import './LoginForm.component.css'
 import InputComponent from '../InputComponent/Input.component'
@@ -38,48 +38,60 @@ function LoginForm() {
         <>
             { contextHolder }
 
-            <Form 
-                form={ form } 
-                name='login-form'
-                className='login-form'
-                layout='vertical'
-                initialValues={{
-                    email: '',
-                    password: '',
-                    remember: true 
-                }}
-                onFinish={ onSubmitForm }
-                autoComplete='off'
-            >
+            <div className='login-form-container'>
+                <h1 id='login-form-title'>Login Account</h1>
+                <p id='login-form-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, cupiditate optio? Minima temporibus voluptates repellendus quidem beatae? Sequi officiis cum repellat, neque culpa quo laudantium esse maiores excepturi, debitis distinctio.</p>
+            
+                <Form 
+                    form={ form } 
+                    name='login-form'
+                    className='login-form'
+                    layout='vertical'
+                    initialValues={{
+                        email: '',
+                        password: '',
+                        remember: true 
+                    }}
+                    onFinish={ onSubmitForm }
+                    autoComplete='off'
+                >
 
-                <InputComponent
-                    label='E-mail'
-                    required={ true }
-                    id='email'
-                    placeholder='Enter your e-mail'
-                    type='email'
-                />
+                    <InputComponent
+                        label='E-mail'
+                        required={ true }
+                        id='email'
+                        placeholder='Enter your e-mail'
+                        type='email'
+                    />
 
-                <InputComponent
-                    label='Password'
-                    required={ true }
-                    id='password'
-                    placeholder='Enter your password'
-                    type='password'
-                />
+                    <InputComponent
+                        label='Password'
+                        required={ true }
+                        id='password'
+                        placeholder='Enter your password'
+                        type='password'
+                    />
 
-                <div className='login-form-remember'>
-                    <Form.Item
-                        name='remember'
-                        valuePropName='checked'
-                    >
-                        <Checkbox className='login-form-checkbox'>Keep me signed in</Checkbox>
-                    </Form.Item>
+                    <div className='login-form-remember'>
+                        <Form.Item
+                            name='remember'
+                            valuePropName='checked'
+                        >
+                            <Checkbox className='login-form-checkbox'>Keep me signed in</Checkbox>
+                        </Form.Item>
 
-                    <a className='login-form-forgot' href="#">Forgot your password?</a>
-                </div>
+                        <Tooltip
+                            overlayClassName='login-form-tooltip'
+                            placement='bottom' 
+                            title='Under construction'
+                            color='#DE541E'
+                        >
+                            <a className='login-form-forgot' href="#">Forgot your password?</a>
+                        </Tooltip>
+                    </div>
 
-            </Form>
+                </Form>
+            </div>
         </>
     )
 }
