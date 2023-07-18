14ff8767c5db4d3fe348ab0b95115c3464bfcc2f
biachristie <beatrizchristie@yahoo.com.br>
2023-07-18 13:29:46 -0300

    add AuthContext to HomePage

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,9 +1,21 @@
+import { useContext } from 'react'
+import { Navigate } from 'react-router'
+
+import { AuthContext } from '../../context/auth/auth.context'
+
 function HomePage() {
-    return (
-        <>
-            
-        </>
-    );
+    const { auth } = useContext(AuthContext)
+
+    const renderPage = () => {
+        return (
+            <>
+                Home
+            </>
+        )
+    }
+
+    return auth.isLogged ? renderPage() : <Navigate to={ '/login' } />
+
 }
 
 export default HomePage;
