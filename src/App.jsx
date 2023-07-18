fbc1c3fa3003d448a19d1e59814474ad4127a9df
biachristie <beatrizchristie@yahoo.com.br>
2023-07-18 13:26:56 -0300

    add modifications to LoginPage route and 404 route to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import LoginPage from './pages/Login/Login.page'
 import HomePage from './pages/Home/Home.page'
 import RegisterPage from './pages/Register/Register.page'
 
+import './App.css'
+
 function App() {
     return (
       <Router>
         <Routes>
-          <Route path='/' element={ <LoginPage /> } />
+          <Route path='/' element={ <HomePage /> } />
+          <Route path='/login' element={ <LoginPage /> } />
           <Route path='/register' element={ <RegisterPage /> } />
-          <Route path='/home' element={ <HomePage /> } />
+          <Route path='*' element={ <><p>Página não existe</p></> } />
         </Routes>
       </Router>
   )
