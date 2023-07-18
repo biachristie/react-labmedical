70943930a11174650fdb1577aac0b6128351168e
biachristie <beatrizchristie@yahoo.com.br>
2023-07-18 00:44:24 -0300

    add RegisterForm to RegisterPage

--- src/pages/Register/Register.page.jsx
+++ src/pages/Register/Register.page.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom'
 
 import './Register.style.css'
 
+import RegisterForm from '../../components/RegisterFormComponent/RegisterForm.component'
+
 function RegisterPage() {
     const navigate = useNavigate()
     const redirectToLogin = () => navigate('/')
@@ -13,17 +15,17 @@ function RegisterPage() {
                 <h1 className='register-title'>Santé</h1>
                 <img className='register-logo' src="./logo.png" alt="Santé logo" />
             </section>
-            <section className='register-form'>
-                {/* Inserir RegisterForm */}
+            <section className='register-form-main'>
+                <RegisterForm />
                 <div className='register-signUp'>
-                    <p>Don't have an account?</p>
+                    <p>Already have an account?</p>
                     <Button 
                         className='register-button'
                         type='primary' 
                         htmlType='button'
                         onClick={ redirectToLogin }
                     >
-                        Sign up!
+                        Log in!
                     </Button>
                 </div>
             </section>
