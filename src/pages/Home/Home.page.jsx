c9c1406319bbe33351ff5c98327ecb697133f744
biachristie <beatrizchristie@yahoo.com.br>
2023-07-20 23:21:25 -0300

    add Layout, with Sider, Header, Content and Footer, to HomePage

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,4 +1,7 @@
-import { Navigate } from 'react-router'
+import { Navigate } from 'react-router-dom'
+import { Layout } from 'antd'
+import { Content, Footer, Header } from 'antd/es/layout/layout'
+import Sider from 'antd/es/layout/Sider'
 
 function HomePage() {
     const isLogged = JSON.parse(localStorage.getItem('isLogged'))
@@ -6,7 +9,34 @@ function HomePage() {
     const renderPage = () => {
         return (
             <>
-                Home
+                <Layout hasSider>
+                    <Sider 
+                        className='layout-sider'
+                        collapsible 
+                        defaultCollapsed={ true }
+                        width={ 240 }
+                        collapsedWidth={ 80 }
+                        breakpoint='xxl'
+                        style={{
+                            overflow: 'hidden',
+                            background: '#082F4B'
+                        }}
+                    >
+                    </Sider>
+                    <Layout>
+                        <Header className='layout-header'>
+                        </Header>
+                        <Content className='layout-content'
+                            style={{
+                                margin: '24px 16px 0',
+                                overflow: 'initial',
+                            }}
+                        >
+                        </Content>
+                        <Footer className='layout-footer'>
+                        </Footer>
+                    </Layout>
+                </Layout>
             </>
         )
     }
