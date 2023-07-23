7c80e194ed3aa30dd0f3acb5e0de9a3267567455
biachristie <beatrizchristie@yahoo.com.br>
2023-07-22 23:54:09 -0300

    add LayoutComponent route to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
+import { useContext, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import './App.css'
+import LayoutComponent from './layout/Layout'
 import LoginPage from './pages/Login/Login.page' 
 import HomePage from './pages/Home/Home.page'
 import RegisterPage from './pages/Register/Register.page'
-
-import './App.css'
 
 function App() {
     return (
       <Router>
         <Routes>
-          <Route path='/' element={ <HomePage /> } />
-          <Route path='/login' element={ <LoginPage /> } />
-          <Route path='/register' element={ <RegisterPage /> } />
-          <Route path='*' element={ <><p>Página não existe</p></> } />
+          <Route element={ <LayoutComponent /> }>
+            <Route path='/' element={ <HomePage /> } />
+            <Route path='*' element={ <><p>Página não existe</p></> } />
+          </Route>
+
+            <Route path='/login' element={ <LoginPage /> } />
+            <Route path='/register' element={ <RegisterPage /> } />
         </Routes>
       </Router>
   )
