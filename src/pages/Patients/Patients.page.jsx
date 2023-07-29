37493148ecdfec843a86dbbd0f8fdbba9cef84e7
biachristie <beatrizchristie@yahoo.com.br>
2023-07-28 22:10:27 -0300

    add columns to table in PatientsPage

--- src/pages/Patients/Patients.page.jsx
+++ src/pages/Patients/Patients.page.jsx
@@ -1,5 +1,6 @@
 import { Navigate } from 'react-router-dom'
-import { Table } from 'antd'
+import { Space, Table } from 'antd'
+import { EyeOutlined } from '@ant-design/icons'
 
 import './Patients.page.css'
 
@@ -7,10 +8,79 @@ function PatientsPage() {
     const isLogged = JSON.parse(localStorage.getItem('isLogged'))
     
     const renderPage = () => {
+        const columns = [
+            {
+                key: 'id',
+                title: 'ID',
+                dataIndex: 'id',
+                align: 'center'
+            },
+            {
+                key: 'fullname',
+                title: 'Nome',
+                dataIndex: 'fullname',
+                sorter: (a, b) => a.fullname > b.fullname,
+            },
+            {
+                key: 'age',
+                title: 'Idade',
+                dataIndex: 'age',
+                sorter: (a, b) => a.age > b.age,
+                align: 'center'
+            },
+            {
+                key: 'gender',
+                title: 'Sexo',
+                dataIndex: 'gender',
+                sorter: (a, b) => a.gender > b.gender,
+            },
+            {
+                key: 'cellphone',
+                title: 'Celular',
+                dataIndex: 'cellphone',
+                sorter: (a, b) => a.cellphone > b.cellphone,
+            },
+            {
+                key: 'email',
+                title: 'E-mail',
+                dataIndex: 'email',
+                sorter: (a, b) => a.email > b.email,
+            },
+            {
+                key: 'appointments',
+                title: 'Total de Consultas',
+                render: (_, record) => {
+                    
+                },
+                align: 'center'
+            },
+            {
+                key: 'exams',
+                title: 'Total de Exames',
+                render: (_, record) => {
+                    
+                },
+                align: 'center'
+            },
+            {
+                key:'actions',
+                title: 'Ações',
+                render: (_, record) => (
+                    <Space>
+                        <a href={ `/patientRecord?id=${record.id}` }>
+                            <EyeOutlined className='layout-content-table-eye' />
+                        </a>
+                    </Space>
+                ),
+                align: 'center'
+            }
+        ]
+
         return(
             <>
                 <Table
                     className='layout-content-table-patients'
+                    columns={ columns }
                     rowKey= { (record) => record.id }
                     style={{ tableLayout: 'fixed' }}
                     scroll={{ x: '100%' }}
