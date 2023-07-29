12d85775fa14048e0c18e4cf69c9b0eefbd241bc
biachristie <beatrizchristie@yahoo.com.br>
2023-07-29 17:15:36 -0300

    add conditional rendering to RegisterPatientPage

--- src/pages/RegisterPatient/RegisterPatient.page.jsx
+++ src/pages/RegisterPatient/RegisterPatient.page.jsx
@@ -1,9 +1,15 @@
 function RegisterPatientPage() {
+    const isLogged = JSON.parse(localStorage.getItem('isLogged'))
+    
+    const render = () => {
         return (
             <>
                 
             </>
         )
+    }
+
+    return isLogged ? render() : <Navigate to='/login' />
 }
 
 export default RegisterPatientPage
