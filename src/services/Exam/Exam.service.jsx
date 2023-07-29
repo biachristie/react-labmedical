bd631911671fcbb91aa89b835603f1fbb823009b
biachristie <beatrizchristie@yahoo.com.br>
2023-07-28 23:45:51 -0300

    add create method to ExamService

--- src/services/Exam/Exam.service.jsx
+++ src/services/Exam/Exam.service.jsx
@@ -8,6 +8,19 @@ const Get = () => {
     return fetchExam()
 }
 
+const Create = (data) => {
+    const fetchExam = async() => {
+        await fetch('http://localhost:3000/exams', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify( data )
+        })
+    }
+
+    return fetchExam()
+}
+
 export const ExamService = {
-    Get
+    Get,
+    Create
 }
