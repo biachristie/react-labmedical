bbf74f93d7261944f23a8a4696cdba4cdb525fb3
biachristie <beatrizchristie@yahoo.com.br>
2023-07-28 23:14:28 -0300

    add get method to PatientService

--- src/services/Patient/Patient.service.jsx
+++ src/services/Patient/Patient.service.jsx
@@ -1,6 +1,13 @@
+const Get = () => {
+    const fetchPatient = async() => {
+        const responsePatients = await fetch('http://localhost:3000/patients')
+        const dataPatients = await responsePatients.json()
+        return dataPatients
+    }
+
+    return fetchPatient()
+}
+
 export const PatientService = {
-    Get,
-    Create,
-    Update,
-    Delete
+    Get
 }
