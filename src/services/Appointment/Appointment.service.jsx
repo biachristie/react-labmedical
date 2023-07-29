3d732843f2420c98aeb9deec0bda07763fc522e5
biachristie <beatrizchristie@yahoo.com.br>
2023-07-28 23:30:41 -0300

    add get method to AppointmentService

--- src/services/Appointment/Appointment.service.jsx
+++ src/services/Appointment/Appointment.service.jsx
@@ -1,6 +1,13 @@
+const Get = () => {
+    const fetchAppointment = async() => {
+        const responseAppointments = await fetch('http://localhost:3000/appointments')
+        const dataAppointments = await responseAppointments.json()
+        return dataAppointments
+    }
+
+    return fetchAppointment()
+}
+
 export const AppointmentService = {
-    Get,
-    Create,
-    Update,
-    Delete
+    Get
 }
