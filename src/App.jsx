c3d0ce68806a6495a4c52a4886f5b734c101f9d5
biachristie <beatrizchristie@yahoo.com.br>
2023-07-29 00:47:16 -0300

    add RegisterExamPage route to App

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import PatientsPage from './pages/Patients/Patients.page'
 import PatientRecordPage from './pages/PatientRecord/PatientRecord.page'
 import RegisterPatientPage from './pages/RegisterPatient/RegisterPatient.page'
 import RegisterAppointmentPage from './pages/RegisterAppointment/RegisterAppointment.page'
+import RegisterExamPage from './pages/RegisterExam/RegisterExam.page'
 
 function App() {
   const { setUsersList } = useContext(UsersContext)
@@ -34,6 +35,7 @@ function App() {
             <Route path='/patientRecord' element={ <PatientRecordPage /> } />
             <Route path='/patientRegister' element={ <RegisterPatientPage /> } />
             <Route path='/appointmentRegister' element={ <RegisterAppointmentPage /> } />
+            <Route path='/examRegister' element={ <RegisterExamPage /> } />
             <Route path='*' element={ <><p>Página não existe</p></> } />
           </Route>
 
