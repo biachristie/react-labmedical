bd290acda4e3ff1125891c0c2c4297c15e065a9e
biachristie <beatrizchristie@yahoo.com.br>
2023-07-29 23:49:17 -0300

    add RegisterPatientForm to RegisterPatientPage

--- src/pages/RegisterPatient/RegisterPatient.page.jsx
+++ src/pages/RegisterPatient/RegisterPatient.page.jsx
@@ -1,10 +1,16 @@
+import { Navigate } from "react-router-dom"
+
+import RegisterPatientForm from "../../components/RegisterPatientFormComponent/RegisterPatientForm.component"
+
 function RegisterPatientPage() {
     const isLogged = JSON.parse(localStorage.getItem('isLogged'))
     
     const render = () => {
+        
+
         return (
             <>
-                
+                <RegisterPatientForm />
             </>
         )
     }
