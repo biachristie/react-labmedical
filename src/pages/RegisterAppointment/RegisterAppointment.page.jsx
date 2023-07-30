6c43c03be0fa80e36036146e3d0a9f7623d1f660
biachristie <beatrizchristie@yahoo.com.br>
2023-07-30 14:55:42 -0300

    add title to RegisterAppointmentPage

--- src/pages/RegisterAppointment/RegisterAppointment.page.jsx
+++ src/pages/RegisterAppointment/RegisterAppointment.page.jsx
@@ -1,7 +1,15 @@
+import { useContext, useEffect } from "react"
 import { Navigate } from "react-router-dom"
+
+import { TitlesContext } from '../../context/titles/titles.context'
 
 function RegisterAppointmentPage() {
     const isLogged = JSON.parse(localStorage.getItem('isLogged'))
+
+    const { setTitle } = useContext(TitlesContext)
+    useEffect(() => { 
+        setTitle('Cadastro de Consulta')
+    }, [])
 
     const render = () => {
         return (
