ea01e58773961663cf3192dee2d031d9e6935bcb
biachristie <beatrizchristie@yahoo.com.br>
2023-07-30 18:27:58 -0300

    add conditional rendering to RegisterExamPage

--- src/pages/RegisterExam/RegisterExam.page.jsx
+++ src/pages/RegisterExam/RegisterExam.page.jsx
@@ -1,9 +1,17 @@
+import { Navigate } from "react-router-dom"
+
 function RegisterExamPage() {
+    const isLogged = JSON.parse(localStorage.getItem('isLogged'))
+    
+    const render = () => {
         return (
             <>
                 
             </>
         )
+    }
+
+    return isLogged ? render() : <Navigate to='/login' />
 }
 
 export default RegisterExamPage
