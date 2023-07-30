85d392c6cbb604cc1a35f8a6836cf90d4ec1c718
biachristie <beatrizchristie@yahoo.com.br>
2023-07-29 23:58:58 -0300

    add title to RegisterPatientPage

--- src/pages/RegisterPatient/RegisterPatient.page.jsx
+++ src/pages/RegisterPatient/RegisterPatient.page.jsx
@@ -1,13 +1,16 @@
+import { useContext, useEffect } from "react"
 import { Navigate } from "react-router-dom"
 
+import { TitlesContext } from '../../context/titles/titles.context'
 import RegisterPatientForm from "../../components/RegisterPatientFormComponent/RegisterPatientForm.component"
 
 function RegisterPatientPage() {
     const isLogged = JSON.parse(localStorage.getItem('isLogged'))
     
-    const render = () => {
-        
+    const { setTitle } = useContext(TitlesContext)
+    useEffect(() => { setTitle('Cadastro de Paciente') }, [])
 
+    const render = () => {
         return (
             <>
                 <RegisterPatientForm />
