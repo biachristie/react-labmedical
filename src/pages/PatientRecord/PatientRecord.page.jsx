1f4f15839fb87c8e61434be962bbe049860fabea
biachristie <beatrizchristie@yahoo.com.br>
2023-07-30 11:06:55 -0300

    add function to get patient data to PatientRecordPage

--- src/pages/PatientRecord/PatientRecord.page.jsx
+++ src/pages/PatientRecord/PatientRecord.page.jsx
@@ -1,19 +1,26 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Navigate } from "react-router-dom"
 
 import { TitlesContext } from '../../context/titles/titles.context'
+import { PatientService } from "../../services/Patient/Patient.service"
 
 function PatientRecordPage() {
     const isLogged = JSON.parse(localStorage.getItem('isLogged'))
-    
+
     let params = new URL(document.location).searchParams
     let patientId = params.get('id')
-    
+
+    const [patient, setPatient] = useState([])
+
     const { setTitle } = useContext(TitlesContext)
     useEffect(() => {
         setTitle('Detalhes do Paciente')
+
+        if (patientId !== null) { 
+            PatientService.Show(patientId).then(result => setPatient(result))
+        }
     }, [])
-    
+
     const render = () => {
         return (
                 <>
