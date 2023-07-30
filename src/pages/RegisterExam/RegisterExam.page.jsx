d1326d49907d46c3f785aa3cf39971c519c7d8a1
biachristie <beatrizchristie@yahoo.com.br>
2023-07-30 18:29:28 -0300

    add title to RegisterExamPage

--- src/pages/RegisterExam/RegisterExam.page.jsx
+++ src/pages/RegisterExam/RegisterExam.page.jsx
@@ -1,8 +1,14 @@
+import { useContext, useEffect } from "react"
 import { Navigate } from "react-router-dom"
+
+import { TitlesContext } from '../../context/titles/titles.context'
 
 function RegisterExamPage() {
     const isLogged = JSON.parse(localStorage.getItem('isLogged'))
     
+    const { setTitle } = useContext(TitlesContext)
+    useEffect(() => { setTitle('Cadastro de Exame') }, [])
+
     const render = () => {
         return (
             <>
